46940479272fe84a3c3b19d11ec91cc9a6520cda
ST3V3DG <rosycrystal024@gmail.com>
2025-12-17 04:31:14 +0100

    add @vercel/analytics dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${switzer.className} antialiased`}>{children}</body>
+			<body className={`${switzer.className} antialiased`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
